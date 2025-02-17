63840a5b0f0464d922487cc1356f127783ec02be
cath990409@gmail.com <cath990409@gmail.com>
2025-02-17 16:57:03 +0900

    2025/ 0217- jsp 첫수업

---
+++ JspServletBasic1/src/main/webapp/BasicInfo.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title>사용자 정보</title>
+
+<script type="text/javascript">
+
+</script>
+
+<style type="text/css">
+
+</style>
+
+</head>
+
+<body>
+
+	<h1>BasicInfo.jsp</h1>
+	<form action="./basicInfo" method="get">
+		<input type="text" name="myName" value="">
+		<input type="submit" value="내이름 찾기">
+	</form>
+
+
+</body>
+</html>
---
+++ JspServletTest1/src/main/java/stu/edu/servlet/MyIntroduce.java
@@ -0,0 +1,76 @@
+package stu.edu.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.Servlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+public class MyIntroduce implements Servlet{
+
+	
+	ServletConfig config;
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return this.config;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		
+		this.config=config;
+		String nameStr = this.config.getInitParameter("nameStr");
+		String ageStr = this.config.getInitParameter("age");
+		
+		int ageNum = Integer.parseInt(ageStr);
+		
+		System.out.println("내 이름: " + nameStr);
+		System.out.println("내 나이: " + ageNum);
+	}
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) 
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter printWriter = response.getWriter();
+		
+		String htmlStr = "";
+		
+		htmlStr += "<!DOCTYPE html>";
+		htmlStr += "<html>";
+		htmlStr += "<head>";
+		htmlStr += "<meta charset=\"UTF-8\">";
+		htmlStr += "<title> jsp&servlet 학습 </title>";
+		htmlStr += "</head>";
+		htmlStr += "<body>";
+		for(int i = 1 ; i<=6; i++) {
+			htmlStr += "<h"+i+">"+"jsp&servlet 학습"+i+"</h"+i+">";
+		}
+		
+		htmlStr += "</body>";
+		htmlStr += "</html>";
+		printWriter.append(htmlStr);
+	}
+	
+
+
+}
